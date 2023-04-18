654a2cd8a8151d20110b777db7f8ed2b351795dc
DylanRothbauer <120593336+DylanRothbauer@users.noreply.github.com>
2023-04-18 18:44:29 -0500

    Add project files.

---
+++ TrinarySearch.cpp
@@ -0,0 +1,109 @@
+#include "TrinarySearch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void testOne() {
+	vector<int> myVec;
+
+	for (int i = 0; i < 10; i++) {
+		myVec.push_back(i);
+	}
+
+	cout << endl;
+	int size = myVec.size();
+
+	printTable(size, myVec, 6);
+}
+
+void testTwo() {
+	vector<int> myVec;
+
+	for (int i = 0; i < 50; i++) {
+		myVec.push_back(i);
+	}
+
+	cout << endl;
+	int size = myVec.size();
+
+	printTable(size, myVec, 15);
+}
+
+void testThree() {
+	vector<int> myVec;
+
+	for (int i = 0; i < 100; i++) {
+		myVec.push_back(i);
+	}
+
+	cout << endl;
+	int size = myVec.size();
+
+	printTable(size, myVec, 22);
+}
+
+void testFour() {
+	vector<int> myVec;
+
+	for (int i = 0; i < 1000; i++) {
+		myVec.push_back(i);
+	}
+
+	cout << endl;
+	int size = myVec.size();
+
+	printTable(size, myVec, 344);
+}
+
+void testFive() {
+	vector<int> myVec;
+
+	for (int i = 0; i < 10000; i++) {
+		myVec.push_back(i);
+	}
+
+	cout << endl;
+	int size = myVec.size();
+
+	printTable(size, myVec, 344);
+}
+
+void testSix() {
+	vector<int> myVec;
+
+	for (int i = 0; i < 100000; i++) {
+		myVec.push_back(i);
+	}
+
+	cout << endl;
+	int size = myVec.size();
+
+	printTable(size, myVec, 433);
+}
+
+void testSeven() {
+	vector<int> myVec;
+
+	for (int i = 0; i < 1000000; i++) {
+		myVec.push_back(i);
+	}
+
+	cout << endl;
+	int size = myVec.size();
+
+	printTable(size, myVec, 234);
+}
+
+int main() {
+
+	testOne();
+	testTwo();
+	testThree();
+	testFour();
+	testFive();
+	testSix();
+	testSeven();
+
+	// return 0;
+}
---
+++ TrinarySearch.h
@@ -0,0 +1,69 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Binary Search
+int binarySearch(vector<int>& vec, int target) {
+    int count = 0; // using a counter for steps
+    int left = 0; // at the start of the vector
+    int right = vec.size() - 1; // at the end of the vector
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        count++;
+        if (vec[mid] == target) { 
+            cout << "BINARY SEARCH STEPS: " << count << endl;
+            return mid;
+        }
+        else if (vec[mid] < target) { // left becomes mid + 1 (because we already checked if mid was the target
+            left = mid + 1; 
+        }
+        else { // otherwise, the right becomes mid - 1
+            right = mid - 1;
+        }
+    }
+    cout << "BINARY SEARCH STEPS: " << count << endl;
+    return -1;  // target not found
+}
+
+// Trinary Search
+int trinarySearch(vector<int>& vec, int target) {
+    int count = 0; // using a counter for steps
+    int left = 0;
+    int right = vec.size() - 1;
+
+    while (left <= right) {
+        int oneThird = left + (right - left) / 3;
+        int twoThirds = right - (right - left) / 3;
+        count += 2;
+        if (vec[oneThird] == target) {
+            cout << "TRINARY SEARCH STEPS: " << count << endl;
+            return oneThird;
+        }
+        else if (vec[twoThirds] == target) {
+            cout << "TRINARY SEARCH STEPS: " << count << endl;
+            return twoThirds;
+        }
+        else if (target < vec[oneThird]) {
+            right = oneThird - 1;
+        }
+        else if (target > vec[twoThirds]) {
+            left = twoThirds + 1;
+        }
+        else {
+            left = oneThird + 1;
+            right = twoThirds - 1;
+        }
+    }
+    cout << "TRINARY SEARCH STEPS: " << count << endl;
+    return -1;  // not found
+}
+
+// Visual for main
+void printTable(int size, vector<int> myVec, int target) {
+    cout << "INPUT SIZE: " << size << endl;
+    binarySearch(myVec, target);
+    trinarySearch(myVec, target);
+}
